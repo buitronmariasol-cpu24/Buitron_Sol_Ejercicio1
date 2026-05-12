a14923efdca77060f6aae0fce348f2ab0b95b052
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-05-12 11:48:20 -0500

    Tercer commit: cambio de valores dento del arreglo

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -5,6 +5,7 @@ int main()
     char vocales[5]= {'a','e','i','o','u'};
     char *ptr;
     ptr = &vocales[0];
+    *(ptr+3)='x';
     for (int i = 0; i < 5; i++)
     {
         printf("%c ", vocales[i]);
