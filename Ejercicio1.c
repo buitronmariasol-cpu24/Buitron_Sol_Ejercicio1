398fb0c40888ca0a1e595fbf5fa064325da38b64
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-05-12 11:38:13 -0500

    Primer commit: funcion base de las vocales

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -10,7 +10,7 @@ int main()
 
     for (int i = 0; i < 5; i++) 
     {
-        printf("arreglo[%d] =%d\t",i, arreglo[i]);
+        printf("arreglo[%d] =%d ",i, arreglo[i]);
         //ptr = &arreglo[i];
         printf("%d\n", *(ptr++));
     }
---
+++ Ejercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char vocales[5]= {'a','e','i','o','u'};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%c ", vocales[i]);
+    }
+    
+
+}
